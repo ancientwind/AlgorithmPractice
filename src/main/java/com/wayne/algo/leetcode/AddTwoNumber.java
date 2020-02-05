d0b213e9be8b920f6c92d9a8b1fafc54e5527821
zengqw <212331901@logon.ds.ge.com>
2020-02-05 10:58:26 +0800

    fix problem of adding sum

--- src/main/java/com/wayne/algo/leetcode/AddTwoNumber.java
+++ src/main/java/com/wayne/algo/leetcode/AddTwoNumber.java
@@ -23,6 +23,7 @@ class ListNode {
 }
 
 public class AddTwoNumber {
+
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode head = new ListNode(-1);
         ListNode index = head;
@@ -31,14 +32,15 @@ public class AddTwoNumber {
         int digitPlus = 0; // 1 means to add 1 in next digit
         while(l1 != null) {
             if (l2 != null) {
-                sum = l1.val + l2.val;
-                index.next = new ListNode(sum % 10 + digitPlus);
+                sum = l1.val + l2.val + digitPlus;
+                index.next = new ListNode(sum % 10 );
 
                 digitPlus = sum / 10;
                 l2 = l2.next;
             } else {
-                index.next = new ListNode(l1.val + digitPlus);
-                digitPlus = 0;
+                sum = l1.val + digitPlus;
+                index.next = new ListNode(sum % 10);
+                digitPlus = sum /10;
             }
             l1 = l1.next;
             index = index.next;
@@ -68,6 +70,8 @@ public class AddTwoNumber {
         testCase1(addTwoNumber);
         testCase2(addTwoNumber);
         testCase3(addTwoNumber);
+        testCase4(addTwoNumber);
+        testCase5(addTwoNumber);
     }
 
     private static void testCase1(AddTwoNumber addTwoNumber) {
@@ -93,5 +97,22 @@ public class AddTwoNumber {
         System.out.println("the result should be: {7 0 8}");
         System.out.println(result);
     }
+
+    private static void testCase4(AddTwoNumber addTwoNumber) {
+        ListNode l1 = new ListNode(9); l1.next = new ListNode(9);
+        ListNode l2 = new ListNode(9);
+        ListNode result = addTwoNumber.addTwoNumbers(l1, l2);
+        System.out.println("the result should be: {8 0 1}");
+        System.out.println(result);
+    }
+
+    private static void testCase5(AddTwoNumber addTwoNumber) {
+        ListNode l1 = new ListNode(3); l1.next = new ListNode(7);
+        ListNode l2 = new ListNode(9); l2.next = new ListNode(2);
+        ListNode result = addTwoNumber.addTwoNumbers(l1, l2);
+        System.out.println("the result should be: {2 0 1}");
+        System.out.println(result);
+    }
+
 }
 
